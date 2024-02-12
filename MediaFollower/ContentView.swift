029595e1464b76f4550a584b04f0f417c55035d2
Carlos Augusto dos Santos Filho <carlosfilho.canal@icloud.com>
2024-02-12 13:04:21 +0000

    first commit

---
+++ MediaFollower/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MediaFollower
+//
+//  Created by Carlos dos Santos Filho on 12/02/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MediaFollower/MediaFollowerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MediaFollowerApp.swift
+//  MediaFollower
+//
+//  Created by Carlos dos Santos Filho on 12/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct MediaFollowerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
